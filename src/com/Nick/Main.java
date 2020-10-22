0f1b27705b5fc12c2dab7be7a1f85f37cbc2323e
nick whitley <nicrofilm@gmail.com>
2020-10-22 06:53:23 -0400

    Added a file writer to create a text document containing a record of the bank card numbers.

--- src/com/Nick/Main.java
+++ src/com/Nick/Main.java
@@ -1,42 +1,26 @@
 package com.Nick;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     private static ArrayList<String> storedCardNumbers = new ArrayList<String>();
+    private static File myFile = new File("bankCardNumbers.txt");
 
     public static void main(String[] args) {
 
         menu();
 
-//        int numberOfAccountsOpened = 15;
-//        String[] typesOfAccount = {"checking","savings","credit","other"};
-//        String[] locations = {"useast","uswest","alaska","hawaii","other"};
 
-        //testing for multiple cards
-        //Card number creation based on number of accounts opened
-//        for(int i = 1; i<numberOfAccountsOpened;) {
-//            //create card number
-//            String cardNumber = getCardNumber(typesOfAccount[getRandom(0,3)],locations[getRandom(0,4)]);
-//
-//            //check if card number already exists in system(ArrayList<String> storedCardNumbers)
-//            if(storedCardNumbers.contains(cardNumber)) {
-//                break;
-//            } else {
-//                //if it does contain card number, i increments to insure no missed accounts.
-//                i++;
-//                System.out.println(cardNumber);
-//                System.out.println(" ");
-//                storedCardNumbers.add(cardNumber);
-//            }
-//        }
     }
 
     //create menu for checking list of cards or creating a new card
-    public static void menu(){
-
+    public static void menu() {
+        //used to check if client wants to exit program
         boolean exit = false;
 
         System.out.println("___________________\n" +
@@ -47,111 +31,112 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
 
-        if(choice == 1) {
+        if (choice == 1) {
             createNewCard();
-        } else if(choice == 2){
+        } else if (choice == 2) {
             System.out.println("-------------------------");
             System.out.println("There are " + storedCardNumbers.size() + " cards in the system");
             for (int i = 0; i < storedCardNumbers.size(); i++) {
                 System.out.println(storedCardNumbers.get(i).toString());
             }
             System.out.println("-------------------------");
-        } else if(choice == 3) {
+        } else if (choice == 3) {
             System.out.println("Thank you for coming in!");
+            writeFile();
             exit = true;
         } else {
-                System.out.println("Incorrect choice.\n" +
-                        "Please choose a valid option: ");
-                menu();
-            }
+            System.out.println("Incorrect choice.\n" +
+                    "Please choose a valid option: ");
+            menu();
+        }
 
-        if(exit == false) {
+        if (exit == false) {
             System.out.println("Would you like to do anything else?");
             menu();
         } else {
 
         }
-        }
+    }
 
 
     //create new bank card with user params
     public static String createNewCard() {
 //        String accountType = setCardAccountType();
 //        String location = setAccountLocation();
-        String cardNumber = getCardNumber(setCardAccountType(),setAccountLocation());
+        String cardNumber = getCardNumber(setCardAccountType(), setAccountLocation());
         System.out.println("New card successfully created.\n" +
                 "Your new bank card number is: \n" +
                 cardNumber);
         storedCardNumbers.add(cardNumber);
         return cardNumber;
+    }
+
+    public static String setAccountLocation() {
+
+        String location = "other";
+
+        System.out.println("What is your location?\n" +
+                "1. US East\n" +
+                "2. US West\n" +
+                "3. Hawaii\n" +
+                "4. Alaska\n" +
+                "5. Other.");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        if (choice == 1) {
+            location.equalsIgnoreCase("useast");
+        } else if (choice == 2) {
+            location.equalsIgnoreCase("uswest");
+        } else if (choice == 3) {
+            location.equalsIgnoreCase("alaska");
+        } else if (choice == 4) {
+            location.equalsIgnoreCase("hawaii");
+        } else if (choice == 5) {
+            location.equalsIgnoreCase("other");
+        } else {
+            System.out.println("Entered invalid selection.\n" +
+                    "Please enter valid selection.");
+            setCardAccountType();
         }
 
-        public static String setAccountLocation() {
+        return location;
+    }
 
-            String location = "other";
+    //set the card account type for the creatNewCard method to use
+    public static String setCardAccountType() {
 
-            System.out.println("What is your location?\n" +
-                    "1. US East\n" +
-                    "2. US West\n" +
-                    "3. Hawaii\n" +
-                    "4. Alaska\n" +
-                    "5. Other.");
-            Scanner scanner = new Scanner(System.in);
-            int choice = scanner.nextInt();
+        String accountType = "other";
 
-            if (choice == 1) {
-                location.equalsIgnoreCase("useast");
-            } else if (choice == 2) {
-                location.equalsIgnoreCase("uswest");
-            } else if (choice == 3) {
-                location.equalsIgnoreCase("alaska");
-            } else if (choice == 4) {
-                location.equalsIgnoreCase("hawaii");
-            } else if(choice == 5) {
-                location.equalsIgnoreCase("other");
-            }else {
-                System.out.println("Entered invalid selection.\n" +
-                        "Please enter valid selection.");
-                setCardAccountType();
-            }
+        System.out.println("Which type of account do you have?\n" +
+                "1. Checking.\n" +
+                "2. Savings.\n" +
+                "3. Credit.\n" +
+                "4. Other.\n" +
+                "Enter corresponding digit: ");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
 
-            return location;
+        if (choice == 1) {
+            accountType.equalsIgnoreCase("checking");
+        } else if (choice == 2) {
+            accountType.equalsIgnoreCase("savings");
+        } else if (choice == 3) {
+            accountType.equalsIgnoreCase("credit");
+        } else if (choice == 4) {
+            accountType.equalsIgnoreCase("other");
+        } else {
+            System.out.println("Entered invalid selection.\n" +
+                    "Please enter valid selection.");
+            setCardAccountType();
         }
 
-        //set the card account type for the creatNewCard method to use
-        public static String setCardAccountType() {
-
-            String accountType = "other";
-
-            System.out.println("Which type of account do you have?\n" +
-                    "1. Checking.\n" +
-                    "2. Savings.\n" +
-                    "3. Credit.\n" +
-                    "4. Other.\n" +
-                    "Enter corresponding digit: ");
-            Scanner scanner = new Scanner(System.in);
-            int choice = scanner.nextInt();
-
-            if (choice == 1) {
-                accountType.equalsIgnoreCase("checking");
-            } else if (choice == 2) {
-                accountType.equalsIgnoreCase("savings");
-            } else if (choice == 3) {
-                accountType.equalsIgnoreCase("credit");
-            } else if (choice == 4) {
-                accountType.equalsIgnoreCase("other");
-            } else {
-                System.out.println("Entered invalid selection.\n" +
-                        "Please enter valid selection.");
-                setCardAccountType();
-            }
-
-            return accountType;
-        }
+        return accountType;
+    }
 
     //create random number for card
     public static int getRandom(int min, int max) {
-        return (int) (Math.random()*(max - min)) + min;
+        return (int) (Math.random() * (max - min)) + min;
     }
 
     public static String getCardNumber(String accountType, String location) {
@@ -165,11 +150,11 @@ public class Main {
         int lastEightSecond;
 
         //assign a value to each account type
-        if(accountType.equalsIgnoreCase("checking")) {
+        if (accountType.equalsIgnoreCase("checking")) {
             accountKeySet = 4400;
-        } else if(accountType.equalsIgnoreCase("savings")) {
+        } else if (accountType.equalsIgnoreCase("savings")) {
             accountKeySet = 4300;
-        } else if(accountType.equalsIgnoreCase("credit")) {
+        } else if (accountType.equalsIgnoreCase("credit")) {
             accountKeySet = 4200;
         } else {
             accountKeySet = 4000;
@@ -177,9 +162,9 @@ public class Main {
 
 
         //Assign a value to each location of client
-        if(location.equalsIgnoreCase("useast")) {
+        if (location.equalsIgnoreCase("useast")) {
             locationKeySet = 1011;
-        } else if(location.equalsIgnoreCase("uswest")) {
+        } else if (location.equalsIgnoreCase("uswest")) {
             locationKeySet = 2011;
         } else if (location.equalsIgnoreCase("Alaska")) {
             locationKeySet = 3011;
@@ -190,8 +175,8 @@ public class Main {
         }
 
         //create a randomized number for last of card number
-        lastEightFirst = getRandom(1000,9999);
-        lastEightSecond = getRandom(1000,9999);
+        lastEightFirst = getRandom(1000, 9999);
+        lastEightSecond = getRandom(1000, 9999);
 
         //construct card number
         StringBuilder key = new StringBuilder();
@@ -203,5 +188,30 @@ public class Main {
         key.append(" ");
         key.append(lastEightSecond);
         return key.toString();
+    }
+
+    public void createFile() {
+        try {
+            if (myFile.createNewFile()) {
+                System.out.println("File created: " + myFile.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("Error when creating file!");
+            e.getMessage();
+        }
+    }
+
+    public static void writeFile() {
+        try {
+            FileWriter myWriter = new FileWriter(myFile.getName());
+            for(String str: storedCardNumbers) {
+                myWriter.write(str + System.lineSeparator());
+            }
+            myWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
